7b41d68027644f98a9f71fb6ea34996e2aa60a38
Cameron Leverett <camrbo@gmail.com>
2023-10-19 19:49:59 -0700

    test CI

---
+++ supabase/seed.sql
@@ -0,0 +1,2 @@
+insert into auth.users (instance_id, id, aud, role, email, encrypted_password, raw_app_meta_data, raw_user_meta_data, email_confirmed_at, created_at)
+  values ('00000000-0000-0000-0000-000000000000', '185f2f83-d63a-4c9b-b4a0-7e4a885799e2', 'authenticated', 'authenticated', 'camrbo@gmail.com', 'pass123', '{"provider":"email","providers":["email"]}', '{}', timezone('utc'::text, now()), timezone('utc'::text, now()));
