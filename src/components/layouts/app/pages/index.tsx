dc6ed86399d8e3f3463ddf536beb0a631ff207d4
Cameron Leverett <camrbo@gmail.com>
2024-01-17 18:17:29 -0800

    added title and logo to landing page

--- src/components/layouts/app/pages/index.tsx
+++ src/components/layouts/app/pages/index.tsx
@@ -1,14 +1,31 @@
 import { Button } from "@nextui-org/react";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 export default function AppHomePage() {
   const router = useRouter();
   return (
-    <div>
-      <h1>Home</h1>
-      <Button radius="sm" color="primary" onClick={() => router.push("/tasks")}>
-        Get Started
-      </Button>
+    <div className="p-4 h-screen justify-center flex items-center">
+      <div className="flex-col space-y-4">
+        <div>
+          <div className="flex items-center">
+            <Image
+              src="/images/favicon.png"
+              width={75}
+              height={75}
+              alt={""}
+              className="-rotate-[20deg] translate-x-3 translate-y-2"
+            />
+            <h1 className="text-6xl text-primary-500">Toduit</h1>
+          </div>
+          <p className="text-gray-500">A simple todo app with Next.js, Tailwind CSS and NextUI</p>
+        </div>
+        <div className="flex justify-end">
+          <Button variant="ghost" radius="sm" color="primary" onClick={() => router.push("/tasks")}>
+            Get Started
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
